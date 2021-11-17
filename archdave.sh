7eb845f2394aa266cc27ed5f91ff2b195fb43625
David Giwojno <davegiwo@gmail.com>
2021-11-17 21:07:29 +0100

    now exits main script when preinstall fails

--- archdave.sh
+++ archdave.sh
@@ -2,10 +2,7 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BASENAME="$( basename $SCRIPT_DIR)"
 
-bash $SCRIPT_DIR/0-preinstall.sh
-if [ ! "$?" = "0" ]; then
-  bash $SCRIPT_DIR/functions/exit.sh
-fi
+bash $SCRIPT_DIR/0-preinstall.sh || exit 0
 arch-chroot /mnt /root/$BASENAME/1-setup.sh
 source /mnt/root/$BASENAME/install.conf
 arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$BASENAME/2-user.sh
