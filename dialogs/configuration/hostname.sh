475811e08611fdf419f65e83210a372f3938f808
David Giwojno <davegiwo@gmail.com>
2022-01-21 04:11:33 +0100

    removed hostname duplication

--- dialogs/configuration/hostname.sh
+++ dialogs/configuration/hostname.sh
@@ -10,17 +10,17 @@
 CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 setHostnameMenu() {
-  hostname=$(whiptail --backtitle "${TITLE}" --title "Set Computer Name" --inputbox "" 0 0 "archlinux" 3>&1 1>&2 2>&3)
+  hostname=$(whiptail --backtitle "${TITLE}" --title "Set Computer Name" --inputbox "Enter your hostname" 8 40 "archlinux" 3>&1 1>&2 2>&3)
   if [ ! "$?" = "0" ]; then
     return 1
   fi
   if [[ ! "$hostname" =~ ^[a-zA-Z0-9][a-zA-Z0-9_-]{1,62}$ || "${hostname: -1}" == "-" ]]; then
-    whiptail --backtitle "${TITLE}" --title "Set Computer Name" --msgbox "Invalid Hostname\nOnly letters, numbers, underscore and hyphen are allowed, minimal of two characters" 0 0
+    whiptail --backtitle "${TITLE}" --title "Set Computer Name" --msgbox "Invalid Hostname\nOnly letters, numbers, underscore and hyphen are allowed, minimal of two characters" 8 40
     setHostnameMenu
     return "$?"
   fi
   if [ "$hostname" = "localhost" ]; then
-    whiptail --backtitle "${TITLE}" --title "Set Computer Name" --msgbox "localhost is not allowed as hostname" 0 0
+    whiptail --backtitle "${TITLE}" --title "Set Computer Name" --msgbox "localhost is not allowed as hostname" 8 40
     setHostnameMenu
     return "$?"
   fi
