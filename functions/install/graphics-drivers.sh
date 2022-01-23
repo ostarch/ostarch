e1597a9b0bf89764b439924087ee8b6fd49a818e
David Giwojno <davegiwo@gmail.com>
2022-01-23 23:35:54 +0100

    added quiet flag to grep

--- functions/install/graphics-drivers.sh
+++ functions/install/graphics-drivers.sh
@@ -15,17 +15,17 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 
-if lspci | grep -E "NVIDIA|GeForce"; then
+if lspci | grep -Eq "NVIDIA|GeForce"; then
 	pacman -S nvidia nvidia-lts nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia --noconfirm --needed
 	nvidia-xconfig
 	echo "options nvidia_drm modeset=1" > /usr/lib/modprobe.d/nvidia-drm.conf
 	#cp "$SCRIPT_DIR/nvidia.conf" "/etc/X11/xorg.conf.d/"
 fi
 
-if lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+if lspci | grep 'VGA' | grep -Eq "Radeon|AMD"; then
 	pacman -S xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon --noconfirm --needed
 fi
 
-if lspci | grep "VGA" | grep "Intel" | grep "Graphics"; then
+if lspci | grep "VGA" | grep "Intel" | grep -q "Graphics"; then
 	pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
 fi
--- functions/install/microcode.sh
+++ functions/install/microcode.sh
@@ -9,11 +9,11 @@
 #--------------------------------------------------------------------
 CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if lscpu | grep "GenuineIntel"; then
+if lscpu | grep -q "GenuineIntel"; then
     echo "Installing Intel microcode"
     pacman -S --noconfirm intel-ucode
     proc_ucode=intel-ucode.img
-elif lscpu | grep "AuthenticAMD"; then
+elif lscpu | grep -q "AuthenticAMD"; then
     echo "Installing AMD microcode"
     pacman -S --noconfirm amd-ucode
     proc_ucode=amd-ucode.img
