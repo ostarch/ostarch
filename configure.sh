69e45f2eca958c961f1ce0648883bcb6d7aa8bc8
David Giwojno <davegiwo@gmail.com>
2022-01-23 20:16:42 +0100

    added zsh configuration menu

--- configure.sh
+++ configure.sh
@@ -10,64 +10,81 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$SCRIPT_DIR/dialogs/menu.sh"
 
-index=0
+zsh="Zsh Configuration + dotfiles"
+grub="Grub Customization"
+keyboard="Set Keyboard Layout"
+locale="Set Locale and Timezone"
+hostname="Set Hostname"
+shutdown_timeout="Set Systemd Shutdown Timeout"
+mirrors="Update System Mirrors"
+pacman="Pacman Customization"
+kde_import="Import KDE Configuration"
+kde_export="Export KDE Configuration"
+add_user="Add User"
+
+nextItem="$zsh"
 configurationMenu() {
   rm "$SCRIPT_DIR/install.conf" 2> /dev/null
   options=()
-  options+=("Grub Customization" "") 
-  options+=("Set Keyboard Layout" "")
-  options+=("Set Locale and Timezone" "")
-  options+=("Set Hostname" "")
-  options+=("Set Systemd Shutdown Timeout" "")
-  options+=("Update Mirrors" "")
-  options+=("Pacman Customization" "")
-  options+=("Import KDE Configuration" "")
-  options+=("Export KDE Configuration" "")
-  options+=("Add User" "")
-  sel=$(whiptail --backtitle "$TITLE" --title "Configuration Menu" --menu "" --cancel-button "Exit" 0 0 0 "${options[@]}" --default-item "${options[$index]}" 3>&1 1>&2 2>&3)
+  options+=("$zsh" "") 
+  options+=("$grub" "") 
+  options+=("$keyboard" "")
+  options+=("$locale" "")
+  options+=("$hostname" "")
+  options+=("$shutdown_timeout" "")
+  options+=("$mirrors" "")
+  options+=("$pacman" "")
+  options+=("$kde_import" "")
+  options+=("$kde_export" "")
+  options+=("$add_user" "")
+  sel=$(whiptail --backtitle "$TITLE" --title "Configuration Menu" --menu "" --cancel-button "Exit" 0 0 0 "${options[@]}" --default-item "$nextItem" 3>&1 1>&2 2>&3)
   if [ ! "$?" = "0" ]; then
     exit
   fi
   case ${sel} in
+    "Zsh Configuration + dotfiles")
+      "$SCRIPT_DIR/functions/dotfiles.sh"
+      nextItem="$grub"
+      ;;
     "Grub Customization")
       "$SCRIPT_DIR/functions/grub.sh"
-      index=2
+      nextItem="$keyboard"
       ;;
     "Set Keyboard Layout")
       "$SCRIPT_DIR/functions/keyboard-layout.sh"
-      index=4
+      nextItem="$locale"
       ;;
     "Set Locale and Timezone")
       "$SCRIPT_DIR/functions/locale.sh"
-      index=6
+      nextItem="$hostname"
       ;;
     "Set Hostname")
       "$SCRIPT_DIR/functions/sethostname.sh"
-      index=8
+      nextItem="$shutdown_timeout"
       ;;
     "Set Systemd Shutdown Timeout")
       "$SCRIPT_DIR/functions/shutdown-timeout.sh"
-      index=10
+      nextItem="$mirrors"
       ;;
     "Update Mirrors")
       "$SCRIPT_DIR/functions/mirrors.sh"
-      index=12
+      nextItem="$pacman"
       ;;
     "Pacman Customization")
       "$SCRIPT_DIR/functions/pacman.sh"
-      index=14
+      nextItem="$kde_import"
       ;;
     "Import KDE Configuration")
       "$SCRIPT_DIR/functions/kde-import.sh"
-      index=16
+      nextItem="$kde_export"
       ;;
     "Export KDE Configuration")
       "$SCRIPT_DIR/functions/kde-export.sh"
-      index=18
+      nextItem="$add_user"
       ;;
     "Add User")
       "$SCRIPT_DIR/functions/adduser.sh"
-      index=0
+      nextItem="$zsh"
       ;;
   esac
 }
