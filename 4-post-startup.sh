457a13e9fb45eefe07e96b37ed66dd0c640d15a3
David Giwojno <davegiwo@gmail.com>
2022-02-02 20:39:51 +0100

    removed not needed locale setup from post startup script

--- 4-post-startup.sh
+++ 4-post-startup.sh
@@ -14,8 +14,6 @@ if [ $(whoami) = "root"  ]; then
   exit
 fi
 
-$SCRIPT_DIR/functions/locale.sh
-
 echo -ne "
 --------------------------------------------------------------------
                          Final Configuration
