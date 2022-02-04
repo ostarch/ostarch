f9288686216b858b780232a7cd5a3e43bbb3c132
David Giwojno <davegiwo@gmail.com>
2022-02-04 01:18:04 +0100

    ignoring output of cat

--- archdave.sh
+++ archdave.sh
@@ -3,14 +3,17 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASENAME="$( basename $SCRIPT_DIR)"
 
 cleanLog() {
-  cat "$1" | $SCRIPT_DIR/functions/filter.sh | tee "$1"
+  if [ ! -f "$1" ]; then
+    return
+  fi
+  cat "$1" | $SCRIPT_DIR/functions/filter.sh | tee "$1" &> /dev/null
   sed -i "/\r/d" "$1"
   sed -i '/^\s*$/d' "$1"
 }
 
 pacman -Sy --noconfirm --needed expect
-script -qec "bash $SCRIPT_DIR/0-preinstall.sh" -O $SCRIPT_DIR/0-preinstall.log || exit 0
-script -qec "arch-chroot /mnt /root/$BASENAME/1-setup.sh" -O $SCRIPT_DIR/1-setup.log || exit 0
+script -qec "bash $SCRIPT_DIR/0-preinstall.sh" -O $SCRIPT_DIR/0-preinstall.log || ( cleanLog $SCRIPT_DIR/0-preinstall.log && exit 0 )
+script -qec "arch-chroot /mnt /root/$BASENAME/1-setup.sh" -O $SCRIPT_DIR/1-setup.log || ( cleanLog $SCRIPT_DIR/1-setup.log && exit 0 )
 source /mnt/root/$BASENAME/install.conf
 script -qec "arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/$BASENAME/2-user.sh" -O $SCRIPT_DIR/2-user.log
 script -qec "arch-chroot /mnt /root/$BASENAME/3-post-setup.sh" -O $SCRIPT_DIR/3-post-setup.log
