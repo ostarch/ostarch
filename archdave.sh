04c573e2d595c8471388375a53993c18cfe75e07
David Giwojno <davegiwo@gmail.com>
2022-02-04 12:36:24 +0100

    created a function to run and log a command

--- archdave.sh
+++ archdave.sh
@@ -11,22 +11,17 @@ cleanLog() {
   sed -i '/^\s*$/d' "$1"
 }
 
-script -qec "bash $SCRIPT_DIR/0-preinstall.sh" -O $SCRIPT_DIR/0-preinstall.log
-if [ ! "$?" = "0" ]; then
-  cleanLog $SCRIPT_DIR/0-preinstall.log
-  exit
-fi
-cleanLog $SCRIPT_DIR/0-preinstall.log
-script -qec "arch-chroot /mnt /root/$BASENAME/1-setup.sh" -O $SCRIPT_DIR/1-setup.log
-if [ ! "$?" = "0" ]; then
-  cleanLog $SCRIPT_DIR/1-setup.log
-  exit
-fi
-cleanLog $SCRIPT_DIR/1-setup.log
+runAndLog() {
+  script -qec "$1" -O "$2"
+  local errorCode="$?"
+  cleanLog "$2"
+  return $errorCode
+}
+
+runAndLog "bash $SCRIPT_DIR/0-preinstall.sh" $SCRIPT_DIR/0-preinstall.log || exit
+runAndLog "arch-chroot /mnt /root/$BASENAME/1-setup.sh" $SCRIPT_DIR/1-setup.log || exit
 source /mnt/root/$BASENAME/install.conf
-script -qec "arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/$BASENAME/2-user.sh" -O $SCRIPT_DIR/2-user.log
-cleanLog $SCRIPT_DIR/2-user.log
-script -qec "arch-chroot /mnt /root/$BASENAME/3-post-setup.sh" -O $SCRIPT_DIR/3-post-setup.log
-cleanLog $SCRIPT_DIR/3-post-setup.log
+runAndLog "arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/$BASENAME/2-user.sh" $SCRIPT_DIR/2-user.log
+runAndLog "arch-chroot /mnt /root/$BASENAME/3-post-setup.sh" $SCRIPT_DIR/3-post-setup.log
 cp $SCRIPT_DIR/*.log /mnt/home/$USERNAME/$BASENAME/
 bash $SCRIPT_DIR/functions/exit.sh 0
