893c7dbc97149425da219321655bb0adb8c40956
David Giwojno <davegiwo@gmail.com>
2022-03-10 00:39:47 +0100

    applying the default wallpaper on first startup

--- functions/wallpaper.sh
+++ functions/wallpaper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #--------------------------------------------------------------------
 #   █████╗ ██████╗  ██████╗██╗  ██╗██████╗  █████╗ ██╗   ██╗███████╗
 #  ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗██║   ██║██╔════╝
@@ -7,43 +7,20 @@
 #  ██║  ██║██║  ██║╚██████╗██║  ██║██████╔╝██║  ██║ ╚████╔╝ ███████╗
 #  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝
 #--------------------------------------------------------------------
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-source $SCRIPT_DIR/install.conf
 
 if [ $(whoami) = "root"  ]; then
   echo "Don't run this as root!"
   exit
 fi
 
-$SCRIPT_DIR/functions/dotfiles.sh
+errors=0
+until plasma-apply-wallpaperimage /usr/share/wallpapers/Next
+do
+  if [[ $errors -ge 10 ]]; then
+    break
+  fi
+  sleep 1.5
+  errors=$((errors+1))
+done
 
-$SCRIPT_DIR/functions/install/yay.sh
-
-echo -ne "
---------------------------------------------------------------------
-                       Installing AUR Packages
---------------------------------------------------------------------
-"
-
-$SCRIPT_DIR/functions/install/install-packages.sh --aur aur
-
-$SCRIPT_DIR/functions/kde-import.sh
-mkdir -p ~/.config/autostart
-cat <<EOF > ~/.config/autostart/wallpaper-post-startup.desktop
-[Desktop Entry]
-Exec=$SCRIPT_DIR/functions/wallpaper.sh
-Icon=dialog-scripts
-Name=wallpaper-post-startup
-Path=
-Type=Application
-X-KDE-AutostartScript=true
-X-KDE-autostart-phase=2
-X-KDE-startup-notify=false
-EOF
-
-echo -ne "
---------------------------------------------------------------------
-                  System ready for 4-post-setup.sh
---------------------------------------------------------------------
-"
+rm ~/.config/autostart/wallpaper-post-startup.desktop &> /dev/null
