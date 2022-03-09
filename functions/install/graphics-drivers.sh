6c8f108b278645349ac53faff33bbadb2f7a8721
David Giwojno <davegiwo@gmail.com>
2022-03-09 17:19:51 +0100

    removed linux-lts and nvidia-lts from minimal install

--- functions/install/graphics-drivers.sh
+++ functions/install/graphics-drivers.sh
@@ -16,13 +16,18 @@ echo -ne "
 "
 
 if lspci | grep -Eq "NVIDIA|GeForce"; then
-	pacman -S --noconfirm --needed nvidia nvidia-lts nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia
+	nvidia_lts=""
+	source "$CURRENT_DIR/../../install.conf" &> /dev/null
+	if [ ! "$INSTALL_TYPE" = "minimal" ]; then
+		nvidia_lts="nvidia-lts"
+	fi
+	pacman -S --noconfirm --needed nvidia $nvidia_lts nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia
 	nvidia-xconfig
 	echo "options nvidia_drm modeset=1" > /usr/lib/modprobe.d/nvidia-drm.conf
 	#cp "$SCRIPT_DIR/nvidia.conf" "/etc/X11/xorg.conf.d/"
 fi
 
-if lspci | grep 'VGA' | grep -Eq "Radeon|AMD"; then
+if lspci | grep "VGA" | grep -Eq "Radeon|AMD"; then
 	pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
 fi
 
