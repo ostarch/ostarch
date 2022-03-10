5b6d90df6a038877eb24f19d8b629c1711a4982b
David Giwojno <davegiwo@gmail.com>
2022-03-10 19:55:34 +0100

    changed description of install type

--- dialogs/configuration/install-type.sh
+++ dialogs/configuration/install-type.sh
@@ -11,9 +11,9 @@ CONFIG_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 setInstallType() {
 	options=()
-  options+=("Full" "(All packages)")
-  options+=("Normal" "(Only minimal AUR packages)")
-  options+=("Minimal" "(Minimal packages)")
+  options+=("Full" "(All packages including IDEs and other AUR software)")
+  options+=("Normal" "(Web browser, utilities, office software, steam)")
+  options+=("Minimal" "(Web browser and basic utilities)")
   result=$(whiptail --backtitle "${TITLE}" --title "Set Install Type" --menu "" --cancel-button "Back" --default-item "Normal" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
   if [ ! "$?" = "0" ]; then
     return 1
