9a80b80b6b570746af81817d383752b1b88e00f3
David Giwojno <davegiwo@gmail.com>
2022-03-12 00:34:33 +0100

    added VM guest tools installation

--- functions/install/vm-guest-tools.sh
+++ functions/install/vm-guest-tools.sh
@@ -9,30 +9,20 @@
 #--------------------------------------------------------------------
 CURRENT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo -ne "
---------------------------------------------------------------------
-                     Installing Graphics Drivers
---------------------------------------------------------------------
-"
-
-if lspci | grep -Eq "NVIDIA|GeForce"; then
-	nvidia_lts=""
-	source "$CURRENT_DIR/../../install.conf" &> /dev/null
-	if [ ! "$INSTALL_TYPE" = "minimal" ]; then
-		nvidia_lts="nvidia-lts"
-	fi
-	pacman -S --noconfirm --needed nvidia $nvidia_lts nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia
-	#nvidia-xconfig
-	echo "options nvidia_drm modeset=1" > /usr/lib/modprobe.d/nvidia-drm.conf
-	#cp "$SCRIPT_DIR/nvidia.conf" "/etc/X11/xorg.conf.d/"
+virt=$(systemd-detect-virt)
+if [ ! "$virt" = "none" ]; then
+    echo "Installing QEMU guest tools"
+    sudo pacman -S --noconfirm --needed qemu-guest-agent spice-vdagent
+    sudo systemctl enable qemu-guest-agent.service
 fi
 
-if lspci | grep "VGA" | grep -Eq "Radeon|AMD"; then
-	pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+if [ "$virt" = "oracle" ]; then
+    echo "Installing VirtualBox guest tools"
+    sudo pacman -S --noconfirm --needed virtualbox-guest-utils
+    sudo systemctl enable vboxservice.service
+elif [ "$virt" = "vmware" ]; then
+    echo "Installing VMware tools"
+    sudo pacman -S --noconfirm --needed open-vm-tools
+    sudo systemctl enable vmtoolsd.service
+    sudo systemctl enable vmware-vmblock-fuse.service
 fi
-
-if lspci | grep "VGA" | grep "Intel" | grep -q "Graphics"; then
-	pacman -S --noconfirm --needed intel-media-driver libva-intel-driver libva-utils vulkan-intel lib32-vulkan-intel
-fi
-
-$CURRENT_DIR/vm-guest-tools.sh
