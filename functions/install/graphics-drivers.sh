3e90bab47b9ff65ca29e47f97d98d2f0611475a2
David Giwojno <davegiwo@gmail.com>
2022-03-13 16:16:29 +0100

    adding packages for VA-API and VDPAU (Hardware video acceleration)

--- functions/install/graphics-drivers.sh
+++ functions/install/graphics-drivers.sh
@@ -21,14 +21,14 @@ if lspci | grep -Eq "NVIDIA|GeForce"; then
 	if [ ! "$INSTALL_TYPE" = "minimal" ]; then
 		nvidia_lts="nvidia-lts"
 	fi
-	pacman -S --noconfirm --needed nvidia $nvidia_lts nvidia-settings nvidia-utils lib32-nvidia-utils lib32-opencl-nvidia
+	pacman -S --noconfirm --needed nvidia $nvidia_lts nvidia-settings nvidia-utils lib32-nvidia-utils
 	nvidia-xconfig
 	echo "options nvidia_drm modeset=1" > /usr/lib/modprobe.d/nvidia-drm.conf
 	#cp "$SCRIPT_DIR/nvidia.conf" "/etc/X11/xorg.conf.d/"
 fi
 
 if lspci | grep "VGA" | grep -Eq "Radeon|AMD"; then
-	pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon
+	pacman -S --noconfirm --needed xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon radeontop
 fi
 
 if lspci | grep "VGA" | grep "Intel" | grep -q "Graphics"; then
