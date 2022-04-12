9dba444f9a09e79618713cbe0d19d7823bed6f30
David Giwojno <davegiwo@gmail.com>
2022-04-12 22:54:45 +0200

    trying to set the wallpaper twice to fix black wallpaper

--- functions/wallpaper.sh
+++ functions/wallpaper.sh
@@ -13,8 +13,12 @@ if [ $(whoami) = "root"  ]; then
   exit
 fi
 
+applyWallpaper() {
+  plasma-apply-wallpaperimage /usr/share/wallpapers/Next
+}
+
 errors=0
-until plasma-apply-wallpaperimage /usr/share/wallpapers/Next
+until applyWallpaper
 do
   if [[ $errors -ge 10 ]]; then
     break
@@ -22,5 +26,8 @@ do
   sleep 1.5
   errors=$((errors+1))
 done
+
+sleep 1.5
+applyWallpaper
 
 rm ~/.config/autostart/wallpaper-post-startup.desktop &> /dev/null
