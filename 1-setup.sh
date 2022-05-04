4cf9c3ec7ba3c1a943590578e522fc338e3c6286
d4ve10 <davegiwo@gmail.com>
2022-05-05 00:41:42 +0200

    adding PLASMA_USE_QT_SCALING to /etc/environment

--- 1-setup.sh
+++ 1-setup.sh
@@ -33,6 +33,7 @@ $SCRIPT_DIR/functions/hostname.sh
 $SCRIPT_DIR/functions/keyboard-layout.sh
 $SCRIPT_DIR/functions/shutdown-timeout.sh
 $SCRIPT_DIR/functions/xdg-portal.sh
+! grep -q "PLASMA_USE_QT_SCALING=1" /etc/environment && echo "PLASMA_USE_QT_SCALING=1" >> /etc/environment
 
 source "$SCRIPT_DIR/install.conf"
 
