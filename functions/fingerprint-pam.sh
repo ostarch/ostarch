d0a6dae55356c16094ed0200df9083856b60f10b
d4ve10 <davegiwo@gmail.com>
2022-05-09 00:46:36 +0200

    adding pam config for fingerprint reader

---
+++ functions/fingerprint-pam.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+#   █████╗ ██████╗  ██████╗██╗  ██╗██████╗  █████╗ ██╗   ██╗███████╗
+#  ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗██║   ██║██╔════╝
+#  ███████║██████╔╝██║     ███████║██║  ██║███████║██║   ██║█████╗  
+#  ██╔══██║██╔══██╗██║     ██╔══██║██║  ██║██╔══██║╚██╗ ██╔╝██╔══╝  
+#  ██║  ██║██║  ██║╚██████╗██║  ██║██████╔╝██║  ██║ ╚████╔╝ ███████╗
+#  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝
+#--------------------------------------------------------------------
+[ "$(id -u)" = "0" ] || exec sudo "$0" "$@"
+
+try_first_pass="auth\t\tsufficient\tpam_unix.so try_first_pass likeauth nullok"
+sufficient_fprintd="auth\t\tsufficient\tpam_fprintd.so"
+
+if type "fprintd-list" &> /dev/null && fprintd-list root 2>/dev/null | grep -vq 'No devices available'; then
+  sed -i.old "1s;^;$sufficient_fprintd\n;" /etc/pam.d/{system-local-login,login,su,sudo,lightdm,kde}
+  sed -i.old "1s;^;$try_first_pass\n;" /etc/pam.d/kde
+fi
