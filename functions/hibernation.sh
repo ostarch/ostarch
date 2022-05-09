48ba4dad84596e1ac895acb85923a87866237882
David Giwojno <davegiwo@gmail.com>
2022-05-09 17:40:31 +0200

    put hibernation parameters in grub config

--- functions/hibernation.sh
+++ functions/hibernation.sh
@@ -16,23 +16,25 @@ if [ "$HIBERNATE_TYPE" == "hibernate" ]; then
   echo "--------------------------------------------------------------------"
   echo "                        Enabling Hibernation                        "
   echo "--------------------------------------------------------------------"
+  configured="false"
   if [[ "$SWAP_TYPE" == "partition" && -n "$SWAP_PARTITION" ]]; then
     swap_uuid=$(blkid -s UUID -o value "$SWAP_PARTITION")
     if [ -n "$swap_uuid" ]; then
-      echo "options resume=UUID=$swap_uuid" > /etc/modprobe.d/hibernate.conf
+      sed -i -E "s/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 resume=UUID=$swap_uuid\"/" /etc/default/grub
+      configured="true"
     fi
   elif [[ "$SWAP_TYPE" == "file" && -f /swapfile ]]; then
     root_uuid=$(blkid -s UUID -o value "$ROOT_PARTITION")
     swap_file_offset=$(filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
     if [[ -n "$root_uuid" && -n "$swap_file_offset" ]]; then
-      echo "options resume=UUID=$root_uuid resume_offset=$swap_file_offset" > /etc/modprobe.d/hibernate.conf
+      sed -i -E "s/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 resume=UUID=$root_uuid resume_offset=$swap_file_offset\"/" /etc/default/grub
+      configured="true"
     fi
   fi
 
-  if [ -f /etc/modprobe.d/hibernate.conf ]; then
+  if [ "$configured" == "true" ]; then
     if ! grep ^HOOKS /etc/mkinitcpio.conf | grep -q resume; then
       sed -i '/^HOOKS/s/filesystems/filesystems resume/' /etc/mkinitcpio.conf
     fi
-    mkinitcpio -P
   fi
 fi
