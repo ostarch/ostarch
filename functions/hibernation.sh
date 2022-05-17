f44bbc3c8b59ee4b8fdce06e9bb23a3513918dbf
David Giwojno <davegiwo@gmail.com>
2022-05-17 15:00:13 +0200

    readding grub resume parameter if set

--- functions/hibernation.sh
+++ functions/hibernation.sh
@@ -16,14 +16,20 @@ if [ "$HIBERNATE_TYPE" == "hibernate" ]; then
   configured="false"
   if [[ "$SWAP_TYPE" == "partition" && -n "$SWAP_PARTITION" ]]; then
     swap_uuid=$(blkid -s UUID -o value "$SWAP_PARTITION")
-    if [ -n "$swap_uuid" ] && ! grep GRUB_CMDLINE_LINUX_DEFAULT /etc/default/grub | grep -q resume; then
+    if [ -n "$swap_uuid" ]; then
+      sed -i -E 's/resume=UUID=.* //' /etc/default/grub
+      sed -i -E 's/ resume=UUID=.*"/"/' /etc/default/grub
       sed -i -E "s/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 resume=UUID=$swap_uuid\"/" /etc/default/grub
       configured="true"
     fi
   elif [[ "$SWAP_TYPE" == "file" && -f /swapfile ]]; then
     root_uuid=$(blkid -s UUID -o value "$ROOT_PARTITION")
     swap_file_offset=$(filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
-    if [[ -n "$root_uuid" && -n "$swap_file_offset" ]] && ! grep GRUB_CMDLINE_LINUX_DEFAULT /etc/default/grub | grep -q resume; then
+    if [[ -n "$root_uuid" && -n "$swap_file_offset" ]]; then
+      sed -i -E 's/resume=UUID=.* //' /etc/default/grub
+      sed -i -E 's/ resume=UUID=.*"/"/' /etc/default/grub
+      sed -i -E 's/resume_offset=.* //' /etc/default/grub
+      sed -i -E 's/ resume_offset=.*"/"/' /etc/default/grub
       sed -i -E "s/GRUB_CMDLINE_LINUX_DEFAULT=\"(.*)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 resume=UUID=$root_uuid resume_offset=$swap_file_offset\"/" /etc/default/grub
       configured="true"
     fi
