cc7818e3dfeca34b2a5141b7e6d159e525cf1a16
d4ve10 <davegiwo@gmail.com>
2022-05-19 18:54:34 +0200

    creating log directory earlier instead of at the end of the script

--- archdave.sh
+++ archdave.sh
@@ -2,6 +2,11 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BASENAME="$( basename $SCRIPT_DIR)"
 
+mkdir "$SCRIPT_DIR/logs" &>/dev/null
+rm "$SCRIPT_DIR"/logs/* &>/dev/null
+current_date=$(date +%d-%m-%y-%T)
+mkdir -p "$SCRIPT_DIR/logs/$current_date" &>/dev/null
+
 cleanLog() {
   if [ ! -f "$1" ]; then
     return
@@ -13,24 +18,20 @@ cleanLog() {
 }
 
 runAndLog() {
-  local logfile="$SCRIPT_DIR/logs/$(basename "$1" .sh).log"
+  local logfile="$SCRIPT_DIR/logs/$current_date/$(basename "$1" .sh).log"
   script -qec "$1" -O "$logfile"
   local errorCode="$?"
   cleanLog "$logfile"
   return $errorCode
 }
 
-mkdir "$SCRIPT_DIR/logs" &>/dev/null
-rm "$SCRIPT_DIR"/logs/* &>/dev/null
 runAndLog "bash $SCRIPT_DIR/0-preinstall.sh" || exit
 runAndLog "arch-chroot /mnt /root/$BASENAME/1-setup.sh" || exit
 runAndLog "arch-chroot /mnt /root/$BASENAME/2-install.sh" || exit
 source /mnt/root/$BASENAME/install.conf
 runAndLog "arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/$BASENAME/3-user.sh" || exit
 runAndLog "arch-chroot /mnt /root/$BASENAME/4-post-setup.sh" || exit
-current_date=$(date +%d-%m-%y-%T)
-mkdir -p /mnt/home/$USERNAME/$BASENAME/logs/$current_date &>/dev/null
-cp -r $SCRIPT_DIR/logs/* /mnt/home/$USERNAME/$BASENAME/logs/$current_date/ &>/dev/null
+cp -r $SCRIPT_DIR/logs/ /mnt/home/$USERNAME/$BASENAME/ &>/dev/null
 if [ "$(stat --printf="%s" "/mnt/home/$USERNAME/$BASENAME/logs/$current_date/0-preinstall.log")" -eq 0 ]; then
   mv /mnt/home/$USERNAME/$BASENAME/logs/0-preinstall.log /mnt/home/$USERNAME/$BASENAME/logs/$current_date/ &>/dev/null
 else
