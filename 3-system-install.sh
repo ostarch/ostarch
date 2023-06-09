38f461aeb9ea8486685be9a77dc5de18b8e33e5c
sajad shafi <sajadwani888@gmail.com>
2023-06-09 22:10:56 +0530

    Update 3-system-install.sh

--- 3-system-install.sh
+++ 3-system-install.sh
@@ -5,17 +5,16 @@ echo "*                   System-Install                    *"
 echo "*                                                     *"
 echo "*******************************************************"
 
+part=1
+pa=1
+
+
 # Skip Pacstrap (Base Install), if Done Earlier:
     set -e
-    if [[ $part -eq 1 ]]; then
-        if [[ $pa -eq 1 ]]; then
-            pacman -Sy git
-        fi
+    if [[ $part -eq 1 ]] && [[ $pa -eq 1 ]]; then
+        echo "Condition 1"
     fi
     set -e
-    if [[ $part -eq 1 ]]; then
-        if [[ $pa -eq 2 ]]; then
-            pacman -Sy neovim
-        fi
+    if [[ $part -eq 1 ]] && [[ $pa -eq 2 ]]; then
+        echo "Condition 2"
     fi
-    echo @Crossed
