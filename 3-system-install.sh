a68bef5c2f1d4897eb6ea3f6291978f9719a654b
sajad shafi <sajadwani888@gmail.com>
2023-06-09 21:14:36 +0530

    Update 3-system-install.sh

--- 3-system-install.sh
+++ 3-system-install.sh
@@ -6,22 +6,9 @@ echo "*                                                     *"
 echo "*******************************************************"
 
 # Skip Pacstrap (Base Install), if Done Earlier:
-    if [[ "$Part" -ne 2  &&  $Pa -eq 2 ]]; then
-            PS5='Skip Pacstrap (Base Install), If Done Earlier? '
-            echo "Skip Pacstrap (Base Install), If Done Earlier?"
-                options=("Yes, Skip Pacstrap (Base Install)" "No, Do Pacstrap (Base Install) Now")
-                select opt in "${options[@]}"
-                do
-                    case $opt in
-                        "Yes, Skip Pacstrap (Base Install)")
-                            BsInst=0
-                            break
-                            ;;
-                        "No, Do Pacstrap (Base Install) Now")
-                            BsInst=1
-                            break
-                            ;;
-                        *) echo "invalid option $REPLY";;
-                    esac
-                done
+    set -e
+    if [ $part -eq 1 ] && [ $pa -eq 1 ]; then
+        echo "Cond 1"
+    elif [ $part -eq 1 ] && [ $pa -eq 2 ]; then
+        echo "Cond 2"
     fi
