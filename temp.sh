1a06d8e3330fedb721afe647017943c1513ffa50
ost <sajadwani888@gmail.com>
2023-06-10 19:35:43 +0530

    ,

--- temp.sh
+++ temp.sh
@@ -9,4 +9,4 @@ echo "*  \____/  |_____/     |_|    /_/    \_\ |_|     \___| |_| |_| *"
 echo "*                                                              *"
 echo "****************************************************************"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # Get the value of Present Directory
-cp -R ${SCRIPT_DIR} /mnt/root/  
+cp -R ${SCRIPT_DIR} /mnt/home/  
