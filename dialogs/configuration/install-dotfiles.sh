076c3c5d7be98f494b67214b97184beadd2d7df8
d4ve10 <davegiwo@gmail.com>
2025-10-30 14:07:52 +0100

    feat: add option not to install dotfiles

--- dialogs/configuration/install-dotfiles.sh
+++ dialogs/configuration/install-dotfiles.sh
@@ -7,8 +7,12 @@
 #  ██║  ██║██║  ██║╚██████╗██║  ██║██████╔╝██║  ██║ ╚████╔╝ ███████╗
 #  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝  ╚═══╝  ╚══════╝
 #--------------------------------------------------------------------
+CONFIG_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-configurationMenuScript() {
-  menuFlow setKeyboardLayoutMenu setLocaleMenu setTimeZoneMenu setDesktopEnvironment setInstallType addUserMenu setUserPasswordMenu setHostnameMenu setInstallDotfilesMenu
-  return "$?"
+setInstallDotfilesMenu() {
+  INSTALL_DOTFILES="no"
+  if (whiptail --backtitle "${TITLE}" --title "Install Dotfiles" --yesno "This will additionally install various dotfiles including Zsh with Oh My Zsh, KDE settings and other miscellaneous configurations" 0 0); then
+    INSTALL_DOTFILES="yes"
+  fi
+  echo "INSTALL_DOTFILES=$INSTALL_DOTFILES" >> "${CONFIG_DIR}/../../install.conf"
 }
